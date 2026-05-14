d71fd359216ded50fb9a8c7b413514dfd7db1d4d
alehegngobezayehu13-hash <alehegngobezayehu13@gmail.com>
2026-05-14 18:51:29 +0300

    Evaluated model performance using accuracy and metrics

--- src/train.py
+++ src/train.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report
+)
 
 data = pd.read_csv("data/churn.csv")
 
@@ -9,7 +14,6 @@ data["churn"] = data["churn"].map({"Yes": 1, "No": 0})
 data = pd.get_dummies(data, columns=["contract_length"])
 
 X = data.drop("churn", axis=1)
-
 y = data["churn"]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -17,7 +21,12 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = LogisticRegression()
-
 model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
 
-print("Model trained successfully!")
+print("Accuracy:", accuracy)
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, predictions))
+print("\nClassification Report:")
+print(classification_report(y_test, predictions))
