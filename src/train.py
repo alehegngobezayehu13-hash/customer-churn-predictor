eb361e9cb774c08303faad55d1fbf574cb88fc32
alehegngobezayehu13-hash <alehegngobezayehu13@gmail.com>
2026-05-14 15:34:10 +0300

    Trained logistic regression model for churn prediction

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 data = pd.read_csv("data/churn.csv")
 
@@ -6,4 +8,16 @@ data["churn"] = data["churn"].map({"Yes": 1, "No": 0})
 
 data = pd.get_dummies(data, columns=["contract_length"])
 
-print(data.head())
+X = data.drop("churn", axis=1)
+
+y = data["churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+
+print("Model trained successfully!")
